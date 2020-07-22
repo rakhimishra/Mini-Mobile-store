68eebefd693361ffab6ee995de0d36a221bd1a26
rakhimishra <mishrarakhi21@gmail.com>
2020-07-22 15:40:45 +0530

    Updates

---
+++ precache-manifest.e251f7d8e9e713db3d5a1206a7ecf0a0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "307f42d873828e6036ba5182a6014a89",
+    "url": "/Mini-Mobile-store/index.html"
+  },
+  {
+    "revision": "9481e149029fda47a18e",
+    "url": "/Mini-Mobile-store/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "612da631f6921524081b",
+    "url": "/Mini-Mobile-store/static/css/main.e5454c30.chunk.css"
+  },
+  {
+    "revision": "9481e149029fda47a18e",
+    "url": "/Mini-Mobile-store/static/js/2.21e23850.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Mini-Mobile-store/static/js/2.21e23850.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "612da631f6921524081b",
+    "url": "/Mini-Mobile-store/static/js/main.314aef16.chunk.js"
+  },
+  {
+    "revision": "c114d74c01227a6668f1",
+    "url": "/Mini-Mobile-store/static/js/runtime-main.13e098ba.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/Mini-Mobile-store/static/media/logo.9f7e506d.svg"
+  }
+]);
